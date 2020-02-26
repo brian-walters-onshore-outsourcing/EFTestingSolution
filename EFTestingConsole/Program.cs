4d323e4beaa35dde2042879d63cc254da7307800
Brian Walters <brian.walters@onshoreoutsourcing.com>
2020-02-26 10:46:45 -0500

    save changes

--- EFTestingConsole/Program.cs
+++ EFTestingConsole/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using SQLTodayLibrary;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,11 +11,28 @@ namespace EFTestingConsole
     {
         static void Main(string[] args)
         {
-            SQLTodayLibrary.sqltodayEntities ctx =
-                new SQLTodayLibrary.sqltodayEntities();
-            foreach( var x in ctx.QEMPs)
-            {
-                Console.WriteLine(x);
+            using (SQLTodayLibrary.sqltodayEntities ctx =
+                new SQLTodayLibrary.sqltodayEntities())
+            { 
+                foreach (var x in ctx.QEMPs)
+                {
+                    Console.WriteLine(x);
+                    x.EMPFNAME = x.EMPFNAME.ToUpper();
+                }
+                Console.WriteLine("********");
+                foreach (var x in ctx.QEMPs)
+                {
+                    Console.WriteLine(x);
+                   
+                }
+                QEMP newitem = new QEMP();
+                newitem.EMPNO = 18;
+                newitem.EMPFNAME = "Brian";
+                newitem.DEPTNAME = "Books";
+                newitem.EMPSALARY = 20;
+                newitem.BOSSNO = 1;
+               // ctx.QEMPs.Add(newitem);
+                ctx.SaveChanges();
             }
         }
     }
