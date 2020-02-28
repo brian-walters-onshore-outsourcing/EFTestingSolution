a64c70c3e6dee533613206f50d24ba799f988cf9
Brian Walters <brian.walters@onshoreoutsourcing.com>
2020-02-28 08:59:54 -0500

    just prior to linq lecture

--- EFTestingConsole/Program.cs
+++ EFTestingConsole/Program.cs
@@ -13,11 +13,15 @@ namespace EFTestingConsole
         {
             using (SQLTodayLibrary.sqltodayEntities ctx =
                 new SQLTodayLibrary.sqltodayEntities())
-            { 
+            {
+                ctx.Database.Log = (x)=>Console.Write(x);
                 foreach (var x in ctx.QEMPs)
                 {
                     Console.WriteLine(x);
-                    x.EMPFNAME = x.EMPFNAME.ToUpper();
+                    if (1 == (x.EMPNO % 2))
+                    {
+                        x.EMPFNAME = x.EMPFNAME.ToUpper();
+                    }
                 }
                 Console.WriteLine("********");
                 foreach (var x in ctx.QEMPs)
@@ -25,14 +29,16 @@ namespace EFTestingConsole
                     Console.WriteLine(x);
                    
                 }
-                QEMP newitem = new QEMP();
-                newitem.EMPNO = 18;
-                newitem.EMPFNAME = "Brian";
-                newitem.DEPTNAME = "Books";
-                newitem.EMPSALARY = 20;
-                newitem.BOSSNO = 1;
-               // ctx.QEMPs.Add(newitem);
+                //QEMP newitem = new QEMP();
+                //newitem.EMPNO = 18;
+                //newitem.EMPFNAME = "Brian";
+                //newitem.DEPTNAME = "Books";
+                //newitem.EMPSALARY = 20;
+                //newitem.BOSSNO = 1;
+                //ctx.QEMPs.Add(newitem);
+                Console.WriteLine("**********=============*************");
                 ctx.SaveChanges();
+                
             }
         }
     }
