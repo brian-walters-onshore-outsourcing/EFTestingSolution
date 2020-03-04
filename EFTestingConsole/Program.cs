3909bfe8bbb671031a4e58de726a7365b4ed1c9b
Brian Walters <brian.walters@onshoreoutsourcing.com>
2020-03-04 10:14:16 -0500

    extention methods created

--- EFTestingConsole/Program.cs
+++ EFTestingConsole/Program.cs
@@ -25,68 +25,146 @@ namespace EFTestingConsole
             return $"Data: int:{intData} string:'{stringData}'  char:'{charData}' double:{doubleData}";
         }
     }
+
+    public static class contextextender
+    {
+ 
+        static public void 
+            viewentry<T>(this System.Data.Entity.Infrastructure.DbEntityEntry<T> e) 
+            where T: class
+
+        {
+            Console.Write($"entry:{e.State,-11} [");
+
+
+
+            switch (e.State)
+            {
+                default:
+                    Console.Write($" Invalid State for this entry ");
+                    break;
+                case (System.Data.Entity.EntityState.Detached):
+                    
+                        Console.Write($" Not Tracking Properties for detached entry ");
+                   
+                    break;
+                case (System.Data.Entity.EntityState.Modified):
+                
+                    foreach (var c in e.OriginalValues.PropertyNames)
+                    {
+                        Console.Write($"{c}:{e.OriginalValues[c]}->{e.CurrentValues[c]} ");
+                    }
+                    break;
+                case (System.Data.Entity.EntityState.Unchanged):
+                    foreach (var c in e.OriginalValues.PropertyNames)
+                    {
+                        Console.Write($"{c}:{e.OriginalValues[c]} ");
+                    }
+                    break;
+                case (System.Data.Entity.EntityState.Added):
+                    foreach (var c in e.CurrentValues.PropertyNames)
+                    {
+                        Console.Write($"{c}:NA->{e.CurrentValues[c]} ");
+                    }
+
+                    break;
+                case (System.Data.Entity.EntityState.Deleted):
+                    foreach (var c in e.OriginalValues.PropertyNames)
+                    {
+                        Console.Write($"{c}:{e.OriginalValues[c]}->NA ");
+                    }
+
+                    break;
+
+            }
+
+            Console.WriteLine($"]");
+        }
+        static public void viewctx(this sqltodayEntities ctx)
+        {
+            Console.WriteLine("******Context Tracing View **********");
+            Console.WriteLine($"Entry Count: {ctx.ChangeTracker.Entries().Count()}");
+            
+            foreach(var e in ctx.ChangeTracker.Entries() )
+            {
+                Console.Write($"entry:{e.State,-11} [");
+
+
+
+                switch (e.State)
+                {
+                    default:
+                        Console.Write($" Invalid State for this entry ");
+                        break;
+                    case (System.Data.Entity.EntityState.Detached):
+
+                        Console.Write($" Not Tracking Properties for detached entry ");
+
+                        break;
+                    case (System.Data.Entity.EntityState.Modified):
+
+                        foreach (var c in e.OriginalValues.PropertyNames)
+                        {
+                            Console.Write($"{c}:{e.OriginalValues[c]}->{e.CurrentValues[c]} ");
+                        }
+                        break;
+                    case (System.Data.Entity.EntityState.Unchanged):
+                        foreach (var c in e.OriginalValues.PropertyNames)
+                        {
+                            Console.Write($"{c}:{e.OriginalValues[c]} ");
+                        }
+                        break;
+                    case (System.Data.Entity.EntityState.Added):
+                        foreach (var c in e.CurrentValues.PropertyNames)
+                        {
+                            Console.Write($"{c}:NA->{e.CurrentValues[c]} ");
+                        }
+
+                        break;
+                    case (System.Data.Entity.EntityState.Deleted):
+                        foreach (var c in e.OriginalValues.PropertyNames)
+                        {
+                            Console.Write($"{c}:{e.OriginalValues[c]}->NA ");
+                        }
+
+                        break;
+
+                }
+
+                Console.WriteLine($"]");
+
+            }
+            Console.WriteLine("-------------------------------------");
+        }
+    }
     class Program
     {
-
+        
        
 
         static void Main(string[] args)
         {
-            #region initialization
-            int[] integers = new int[] { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9 };
-            List<string> strings = new List<string>();
-            strings.Add("Zero");
-            strings.Add("One");
-            strings.Add("two");
-            strings.Add("three");
-            strings.Add("Four");
-            strings.Add("five");
-            strings.Add("Six");
-            strings.Add("seven");
-            strings.Add("Eight");
-            strings.Add("Nine");
-            List<Data> datas = new List<Data>();
-            datas.Add(new Data(0, "Zero", 'Z', 0.0001));
-            datas.Add(new Data(1, "One", 'O', 1.1111));
-            datas.Add(new Data(2, "two", 't', 2.2222));
-            datas.Add(new Data(3, "three", 't', 3.3333));
-            datas.Add(new Data(4, "Four", 'F', 4.4444));
-            datas.Add(new Data(5, "five", 'f', 5.5555));
-            datas.Add(new Data(6, "Six", 'S', 6.6666));
-            datas.Add(new Data(7, "seven", 's', 7.7777));
-            datas.Add(new Data(8, "Eight", 'E', 8.8888));
-            datas.Add(new Data(9, "Nine", 'N', 9.9999));
-            #endregion initialization
-            QEMP mainemp;
-
+            
             using (sqltodayEntities ctx = new sqltodayEntities())
             {
-               // ctx.Database.Log = Console.Write;
-                Console.WriteLine(ctx.ChangeTracker.Entries().Count());
-                var Q1 = from r in ctx.QEMPs select r;
-                Console.WriteLine(ctx.ChangeTracker.Entries().Count());
-                var L1 = Q1.ToList();
-                Console.WriteLine(ctx.ChangeTracker.Entries().Count());
-                mainemp = L1[0];
-                Console.WriteLine(L1[0]);
-                Console.WriteLine(ctx.ChangeTracker.Entries().Count());
-              //  Console.WriteLine(L1[0].QDEPT.DEPTFLOOR);
-              //foreach (var item in L1)
-              //  {
-              //      Console.WriteLine(item.QDEPT.DEPTFLOOR);
-              //      Console.WriteLine($"Count:{ctx.ChangeTracker.Entries().Count()}");
-              //      foreach(var delitem in item.QDEPT.QDELs)
-              //      {
-              //          Console.WriteLine(delitem.QITEM);
-              //          Console.WriteLine($"Count:{ctx.ChangeTracker.Entries().Count()}");
-              //      }
-              //  }
-                Console.WriteLine(ctx.ChangeTracker.Entries().Count());
+                ctx.viewctx();
+                ctx.Database.Log = Console.WriteLine;
+                QEMP newemp = new QEMP() { DEPTNAME="Books", EMPFNAME="Carol", EMPSALARY=25000, BOSSNO=3, EMPNO = ctx.QEMPs.Max(x=>x.EMPNO)+1 };
+                ctx.viewctx();
+                QEMP stub = new QEMP() { EMPNO = 13 };
+
+                var s = ctx.Entry(stub);
+               // s.State = System.Data.Entity.EntityState.Deleted;
+                Console.WriteLine("Here i am");
+                s.viewentry();
+                Console.WriteLine("There I was");
+                ctx.viewctx();
+
+                var L = ctx.QEMPs.ToList();
+                ctx.QEMPs.Add(newemp);
+                ctx.viewctx();
             }
-
-            Console.WriteLine(mainemp);
-            Console.WriteLine(mainemp.DEPTNAME);
-
+ 
         }
     }
 }
